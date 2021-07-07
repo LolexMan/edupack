19f6236218f7312ccb7006292a39fda717a93770
anthonyramos47 <anthonyramos47@gmail.com>
2021-07-06 20:29:42 -0500

    Test Final

--- test.py
+++ test.py
@@ -4,23 +4,28 @@ from funciones_generales import *
 import time
 
 #------Parametros----------
-total_datos = 10
+total_datos = 100000
 total_notas = 20
 porcentaje_buenos = 0.2
 porcentaje_malos  = 0.2
-ls_ruido = [0.3, 0.4, 0.5]
+ls_ruido = [0.1, 0.2, 0.3, 0.4, 0.5]
 
 tic = time.time()
+ls = []
 for ruido in ls_ruido:
-    dic = generar_datos(total_datos, total_notas, ruido, porcentaje_buenos, ruido, porcentaje_malos, ruido)
-#Visualizacion datos
-#hist_means(dic)
+    acc = 0
+    for i in range(100):
+        dic = generar_datos(total_datos, total_notas, ruido, porcentaje_buenos, ruido, porcentaje_malos, ruido)
+        acc += precision(dic)
+    ls.append( (ruido, acc/100) )
+
+print(ls)
 
 #Aqui vendra la prediccion
-print(precision(dic))
-toc = time.time()
-time = toc - tic 
-print(time)
+
+# toc = time.time()
+# time = toc - tic 
+# print(time)
 
 
 #Aqui se comparara los resultados
